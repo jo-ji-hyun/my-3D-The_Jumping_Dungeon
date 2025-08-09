eaf90d0b17e25dba046463ed433b41675ec6d2ac
jo-ji-hyun <newterr@naver.com>
2025-08-09 15:35:42 +0900

    [Docs]장애물 준비

--- Assets/03.Scripts/UI/HpUi.cs
+++ Assets/03.Scripts/UI/HpUi.cs
@@ -13,29 +13,8 @@ public class HpUi : MonoBehaviour
         hpbar.fillAmount = GetPercentage();
     }
 
-    public void TakeDamage(float damage)
-    {
-        GameManager.Instance.PlayerManager.curHp -= damage;
-
-        if (GameManager.Instance.PlayerManager.curHp <= 0)
-        {
-            // === 플레이어 체력이 0이 되면 GameManager에게 게임 오버 호출 ===
-            GameManager.Instance.GameOver();
-        }
-    }
-
     float GetPercentage()
     {
         return GameManager.Instance.PlayerManager.curHp / GameManager.Instance.PlayerManager.maxHp;
-    }
-
-    public void Add(float value)
-    {
-        GameManager.Instance.PlayerManager.curHp = Mathf.Min(GameManager.Instance.PlayerManager.curHp + value, GameManager.Instance.PlayerManager.maxHp);
-    }
-
-    public void Subtract(float value)
-    {
-        GameManager.Instance.PlayerManager.curHp = Mathf.Max(GameManager.Instance.PlayerManager.curHp - value, 0f);
     }
 }
