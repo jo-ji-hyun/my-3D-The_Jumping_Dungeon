4ca15cc53bf2cd54cfc62cfa322d8b43220e61b9
jo-ji-hyun <newterr@naver.com>
2025-08-11 16:42:10 +0900

    [Fix] 점프 로직 수정

--- Assets/03.Scripts/Npc/Trampoline.cs
+++ Assets/03.Scripts/Npc/Trampoline.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     // === 가해지는 점프력 ===
-    private float _jump_Up = 30.0f;
+    private float _jump_Up = 18.5f;
 
     // === 충돌시 점프 (트리거 x) ===
     private void OnCollisionEnter(Collision collision)
@@ -15,20 +15,12 @@ public class Trampoline : MonoBehaviour
             // === 플레이어의 Rigidbody ===
             Rigidbody _playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
 
-            // === 점프 지점 === 
-            Vector3 contactPoint = collision.contacts[0].point;
-
             // === 기존 점프력 고정 ===
             _playerRigidbody.velocity = new Vector3(_playerRigidbody.velocity.x, 0, _playerRigidbody.velocity.z);
 
-            // === 점프 방향 계산 (반대 방향으로 게산) === 
-            Vector3 jumpDirection = (collision.transform.position - contactPoint).normalized;
-
-            Vector3 jumpping = new(0, jumpDirection.y, 0);
-
             if (_playerRigidbody != null)
             {
-                _playerRigidbody.AddForce(jumpping * _jump_Up, ForceMode.Impulse); // 순간적인 점프
+                _playerRigidbody.AddForce(Vector3.up * _jump_Up, ForceMode.Impulse); // 순간적인 점프
             }
         }
     }
