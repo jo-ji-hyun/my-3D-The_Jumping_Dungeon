7fb005d5d3e48c2be899e188061a52ee5ed71705
jo-ji-hyun <newterr@naver.com>
2025-08-11 13:00:38 +0900

    [Feat]아이템 구역 준비

--- Assets/03.Scripts/Npc/Trampoline.cs
+++ Assets/03.Scripts/Npc/Trampoline.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // === 가해지는 점프력 ===
+    private float _jump_Up = 40.0f;
 
-    // Update is called once per frame
-    void Update()
+    // === 충돌시 점프 (트리거 x) ===
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // === 플레이어의 Rigidbody ===
+            Rigidbody _playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+
+            // === 점프 지점 === 
+            Vector3 contactPoint = collision.contacts[0].point;
+
+            // === 기존 점프력 고정 ===
+            _playerRigidbody.velocity = new Vector3(_playerRigidbody.velocity.x, 0, _playerRigidbody.velocity.z);
+
+            // === 점프 방향 계산 (반대 방향으로 게산) === 
+            Vector3 jumpDirection = (collision.transform.position - contactPoint).normalized;
+
+            Vector3 jumpping = new(0, jumpDirection.y, 0);
+
+            if (_playerRigidbody != null)
+            {
+                _playerRigidbody.AddForce(jumpping * _jump_Up, ForceMode.Impulse); // 순간적인 점프
+            }
+        }
     }
 }
