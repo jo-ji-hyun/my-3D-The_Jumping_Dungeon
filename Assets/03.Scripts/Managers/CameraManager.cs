036dea9964d62e40912a554811c7ee5a7856ad3a
jo-ji-hyun <newterr@naver.com>
2025-08-12 19:37:30 +0900

    [Fix] 이동하는 땅에따라 플레이어의 크기문제를 해결하기 위해 카메라를 떨어뜨림

--- Assets/03.Scripts/Managers/CameraManager.cs
+++ Assets/03.Scripts/Managers/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScale : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
